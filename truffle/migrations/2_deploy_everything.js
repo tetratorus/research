9323e0f51d8ae733acd4d4e6101bade441fbfc14
tetratorus <tetratorus@gmail.com>
2018-08-09 23:55:42 +0800

    Remove extraneous linking in migrations

--- truffle/migrations/2_deploy_everything.js
+++ truffle/migrations/2_deploy_everything.js
@@ -2,7 +2,5 @@ var Schnorr = artifacts.require("Schnorr");
 var Repository = artifacts.require("Repository");
 
 module.exports = function(deployer) {
-  deployer.deploy(Schnorr);
-  deployer.link(Schnorr, Repository);
   deployer.deploy(Repository);
 };
