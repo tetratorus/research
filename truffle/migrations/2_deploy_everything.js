19a6089ec9ba6b3008b2552edc0fae2082ef704e
tetratorus <tetratorus@gmail.com>
2018-08-11 18:46:04 +0800

    Fix issues with Schnorr library not being linked

--- truffle/migrations/2_deploy_everything.js
+++ truffle/migrations/2_deploy_everything.js
@@ -1,5 +1,8 @@
-var Repository = artifacts.require("Repository");
+var Repository = artifacts.require('Repository');
+var Schnorr = artifacts.require('Schnorr')
 
 module.exports = function(deployer) {
+  deployer.deploy(Schnorr);
+  deployer.link(Schnorr, Repository);
   deployer.deploy(Repository);
 };
