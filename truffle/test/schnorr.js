49d55cca1aa8dcb539f82ce4f8bc33b590a95c02
YZhenY <fuz3zy@gmail.com>
2018-08-17 01:59:14 +0800

    abstracted out schnorrs, built utils

--- truffle/test/schnorr.js
+++ truffle/test/schnorr.js
@@ -5,18 +5,9 @@ var ECIES = require('bitcore-ecies')()
 var BN = require('bn.js')
 var EC = artifacts.require('EC')
 var crypto = require('crypto')
-var createKeccakHash = require('keccak')
-
-function keccak256(inp){
-  return createKeccakHash('keccak256').update(inp.toString()).digest('hex')
-}
-
-function random(bytes){
-  do {
-      var k = new BN(crypto.randomBytes(bytes))
-  } while (k.toString() == "0" && k.gcd(ec.curve.n).toString() != "1")
-  return k
-}
+var keccak256 = require('../../utils/keccak256.js');
+var random = require('../../utils/random.js')(ec);
+var schnorr = require('../../src/schnorr.js');
 
 contract('Schnorr Tests', function(accounts) {
   
@@ -26,34 +17,28 @@ contract('Schnorr Tests', function(accounts) {
     var priv = random(32)
     var privC = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
     var y = ec.curve.g.mul(privC)
-    var k = random(32)
-    var r = ec.curve.g.mul(k)
-    var e = keccak256(m + r.getX().toString())
-    var s = k.add(priv.mul(new BN(e, 16)))
+    var schnorrSig = schnorr.sign(m, priv);
     
     // verify
-    assert.equal(e, keccak256(m + y.mul(new BN(e, 16)).add(ec.curve.g.mul(s)).getX().toString()))
+    assert(schnorr.verify(schnorrSig.s, schnorrSig.e, y, m));
   })
-
+  
   it('should sign and verify on-chain', async function() {
     // generate
     var m = "this is a random message"
     var priv = random(32)
     var privC = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
     var y = ec.curve.g.mul(privC)
-    var k = random(32)
-    var r = ec.curve.g.mul(k)
-    var e = keccak256(m + r.getX().toString())
-    var s = k.add(priv.mul(new BN(e, 16))).umod(ec.curve.n)
-    
+    var schnorrSig = schnorr.sign(m, priv);
+
     // verify
     var instance = await EC.deployed()
     var res = await instance.verifySchnorrSignature(
       '0x'+y.getX().toString(16, 64),
       '0x'+y.getY().toString(16, 64),
       m,
-      '0x'+e.toString(16, 64),
-      '0x'+s.toString(16, 64)
+      '0x'+schnorrSig.e.toString(16, 64),
+      '0x'+schnorrSig.s.toString(16, 64)
     )
     assert.equal(res, true)
   })
