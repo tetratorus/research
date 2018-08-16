c477c5085af9ea436f21a72ae220995a5b660486
tetratorus <tetratorus@gmail.com>
2018-08-17 06:59:31 +0800

    Add test for blind schnorr

--- truffle/contracts/EC.sol
+++ truffle/contracts/EC.sol
@@ -92,9 +92,15 @@ contract EC {
     str = string(s);
   }
 
-  function verifySchnorrSignature(uint256 pubX, uint256 pubY, string message, uint256 challenge, uint256 proof) view public returns (bool) {
+  function verifySchnorrSignatureOnMessage(uint256 pubX, uint256 pubY, string message, uint256 challenge, uint256 proof) view public returns (bool) {
     Curve.Point memory sum = Curve.pointAdd(Curve.Point({X: pubX, Y: pubY}).scalarMult(challenge), Curve.scalarBaseMult(proof)); // y^e*g^s = r
     uint256 projection = sum.X % Curve.genOrder();
     return (challenge == uint256(keccak256(abi.encodePacked(message,uintToString(projection))))); // e = H(m, r)
+  }
+
+  function verifySchnorrSignature(uint256 pubX, uint256 pubY, uint256 r, uint256 challenge, uint256 proof) view public returns (bool) {
+    Curve.Point memory sum = Curve.pointAdd(Curve.Point({X: pubX, Y: pubY}).scalarMult(challenge), Curve.scalarBaseMult(proof)); // y^e*g^s = r
+    uint256 projection = sum.X % Curve.genOrder();
+    return (projection == r);
   }
 }
--- truffle/test/schnorr.js
+++ truffle/test/schnorr.js
@@ -15,7 +15,7 @@ function random(bytes){
   do {
       var k = new BN(crypto.randomBytes(bytes))
   } while (k.toString() == "0" && k.gcd(ec.curve.n).toString() != "1")
-  return k
+  return k.umod(ec.curve.n)
 }
 
 contract('Schnorr Tests', function(accounts) {
@@ -24,8 +24,8 @@ contract('Schnorr Tests', function(accounts) {
     // generate
     var m = "this is a random message"
     var priv = random(32)
-    var privC = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
-    var y = ec.curve.g.mul(privC)
+    var privInv = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
+    var y = ec.curve.g.mul(privInv)
     var k = random(32)
     var r = ec.curve.g.mul(k)
     var e = keccak256(m + r.getX().toString())
@@ -39,8 +39,8 @@ contract('Schnorr Tests', function(accounts) {
     // generate
     var m = "this is a random message"
     var priv = random(32)
-    var privC = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
-    var y = ec.curve.g.mul(privC)
+    var privInv = ec.curve.n.sub(priv).umod(ec.curve.n)
+    var y = ec.curve.g.mul(privInv)
     var k = random(32)
     var r = ec.curve.g.mul(k)
     var e = keccak256(m + r.getX().toString())
@@ -48,18 +48,80 @@ contract('Schnorr Tests', function(accounts) {
     
     // verify
     var instance = await EC.deployed()
-    var res = await instance.verifySchnorrSignature(
-      '0x'+y.getX().toString(16, 64),
-      '0x'+y.getY().toString(16, 64),
+    var res = await instance.verifySchnorrSignatureOnMessage(
+      '0x' + y.getX().toString(16, 64),
+      '0x' + y.getY().toString(16, 64),
       m,
-      '0x'+e.toString(16, 64),
-      '0x'+s.toString(16, 64)
+      '0x' + e.toString(16, 64),
+      '0x' + s.toString(16, 64)
     )
     assert.equal(res, true)
   })
 
-  // it should blind schnorr sign
-  // it should blind schnorr verify
+  it('should blind schnorr sign and verify', async function() {
+    // generate params
+    var m = "this is a random message"
+    var priv = random(32)
+    var privInv = ec.curve.n.sub(priv).umod(ec.curve.n)
+    var y = ec.curve.g.mul(privInv)
+
+    // generate commitment
+    var k = random(32)
+    var r = ec.curve.g.mul(k)
+
+    // generate blinding params
+    var alpha = random(32)
+    var beta = random(32)
+
+    // blind commitment
+    var rprime = r.add(ec.curve.g.mul(alpha)).add(y.mul(beta))
+    var eprime = (new BN(keccak256(m + rprime.getX().toString()), 16)).umod(ec.curve.n)
+    var e = eprime.sub(beta).umod(ec.curve.n)
+
+    // sign
+    var s = k.add(priv.mul(e)).umod(ec.curve.n)
+
+    // verify blind schnorr
+    assert.equal(ec.curve.g.mul(s).add(y.mul(e)).getX().toString(16, 64), r.getX().toString(16, 64))
+
+    // verify blind schnorr on-chain
+    var instance = await EC.deployed()
+    var res = await instance.verifySchnorrSignature(
+      '0x' + y.getX().toString(16, 64),
+      '0x' + y.getY().toString(16, 64),
+      '0x' + r.getX().toString(16, 64),
+      '0x' + e.toString(16, 64),
+      '0x' + s.toString(16, 64)
+    )
+    assert.equal(res, true)
+  
+    // deblind
+    sprime = s.add(alpha).umod(ec.curve.n)
+
+    // verify deblinded schnorr
+    assert.equal(ec.curve.g.mul(sprime).add(y.mul(eprime)).getX().toString(16, 64), rprime.getX().toString(16, 64))
+
+    // verify deblinded schnorr on-chain
+    var res2 = await instance.verifySchnorrSignature(
+      '0x' + y.getX().toString(16, 64),
+      '0x' + y.getY().toString(16, 64),
+      '0x' + rprime.getX().toString(16, 64),
+      '0x' + eprime.toString(16, 64),
+      '0x' + sprime.toString(16, 64)
+    )
+    assert.equal(res2, true)
+
+    var res3 = await instance.verifySchnorrSignatureOnMessage(
+      '0x' + y.getX().toString(16, 64),
+      '0x' + y.getY().toString(16, 64),
+      m,
+      '0x' + eprime.toString(16, 64),
+      '0x' + sprime.toString(16, 64)
+    )
+    assert.equal(res3, true)
+
+  })
+
   // it should blind schnorr verify on-chain
   // it should deblind and schnorr verify
   // it should deblind and schnorr verify on-chain
