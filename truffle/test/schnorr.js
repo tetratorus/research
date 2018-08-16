20b65c5eaffe104c61b383180ea42d2b0fda2dde
tetratorus <tetratorus@gmail.com>
2018-08-17 00:15:49 +0800

    Modify schnorr sign and verify to fit the usual y = g^-x form

--- truffle/test/schnorr.js
+++ truffle/test/schnorr.js
@@ -24,11 +24,12 @@ contract('Schnorr Tests', function(accounts) {
     // generate
     var m = "this is a random message"
     var priv = random(32)
-    var y = ec.curve.g.mul(priv)
+    var privC = ec.curve.n.sub(priv).umod(ec.curve.n) // note: inverse is using n
+    var y = ec.curve.g.mul(privC)
     var k = random(32)
     var R = ec.curve.g.mul(k)
     var r = keccak256(m + R.getX().toString())
-    var s = k.sub(priv.mul(new BN(r, 16)))
+    var s = k.add(priv.mul(new BN(r, 16)))
     // verify
     assert.equal(r, keccak256(m + y.mul(new BN(r, 16)).add(ec.curve.g.mul(s)).getX().toString()))
   })
