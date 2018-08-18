87e546fe219b8b2c81395a5c9cf523fc704ab574
tetratorus <tetratorus@gmail.com>
2018-08-18 18:38:41 +0800

    Fix linting

--- utils/random.js
+++ utils/random.js
@@ -5,7 +5,7 @@ module.exports = (ec) => {
   return function random (bytes) {
     do {
       var k = new BN(crypto.randomBytes(bytes))
-    } while (k.toString() == '0' && k.gcd(ec.curve.n).toString() != '1')
+    } while (k.toString() === '0' && k.gcd(ec.curve.n).toString() !== '1')
     return k
   }
 }
